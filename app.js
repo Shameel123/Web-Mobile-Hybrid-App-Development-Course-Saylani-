2590e164988cfc62364faa6769f4e359e9394fb2
Shameel123 <45264147+Shameel123@users.noreply.github.com>
2020-06-21 18:21:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// Q#1
+// var a = new Date();
+// document.write(a);
+
+//Q#2
+// function fullName() {
+//      var firstName = "Zunair";
+//      var lastName = "khan";
+//      alert("Full Name is :"+ " " + firstName + " "+ lastName);
+// }
+// fullName();
+
+//Q#3
+// function Sum(a,b){
+//     var z = a+b
+//     return z;
+// }
+// var c = Sum(4,8);
+// alert(c);
+
+//Q#4
+// function calc(num1,opr,num2){
+//     if(opr === "+"){
+//         return num1 + num2
+//     }
+//     else if(opr === "-"){
+//         return num1 - num2
+//     }
+//     else if(opr === "*"){
+//         return num1 * num2
+//     }
+//     else{
+//         return "Incorrect Operator!"
+//     }
+// }
+// var result = calc(5,"+",5);
+// var result1 = calc(4,"*",6);
+// var result2 = calc(4,"$",6);
+// document.write(result +" ")
+// document.write(result1 +"  ")
+// document.write(result2)
+
+//Q#5
+// function squa(a){
+//    var b = a*a;
+//    document.write(b)
+// }
+// squa(4)
+
+//Q#6
+// function fact(n){
+//     var b = n * (n-1) *(n-2);
+//     document.write(b) 
+// }
+// fact(4)
+
